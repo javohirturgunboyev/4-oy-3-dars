506d72b458151fda3a1634175e228573e0b9d8a0
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-22 13:56:43 +0500

    tugadi

--- index.js
+++ index.js
@@ -1,12 +1,270 @@
 // 1-masala
-let a = +prompt("son kiriting");
-let musbat;
-let manfiy;
-if (a>0) {
-    musbat = a;
-} else(a<0) {
-manfiy = a;
-}
+// let a = +prompt("son kiriting");
+// let musbat;
+// let manfiy;
+// if (a>0) {
+//     musbat = a;
+// } else {
+// manfiy = a;
+// }
 
-console.log(musbat);
-console.log(manfiy);
+// console.log(musbat);
+// console.log(manfiy);
+
+// 2-masala
+
+// let b = +prompt("son kiriting");
+// let toq_son;
+// let juft_son;
+
+// if (b % 2 ==1) {
+//     toq_son = b;
+// } else {
+// juft_son = b;
+// }
+
+// console.log(toq_son);
+// console.log(juft_son);
+
+// 3-masala
+// let c = +prompt("son kiriting");
+// let juft_son;
+// let toq_son;
+
+
+// if (c % 2 ==0) {
+  // juft_son = c;
+// } else {
+// toq_son = c;
+
+// }
+
+// console.log(juft_son);
+// console.log(toq_son);
+
+// 4-masala
+
+// let d = +prompt("son kiriting");
+// let e = +prompt("son kiriting");
+
+// let true;
+// let false;
+
+// if (d>2 && e<=3) {
+//     true;
+// } else{
+// false;
+
+// }
+
+// console.log(true);
+// console.log(false);
+
+// 5-masala
+
+// let d = +prompt("son kiriting");
+// let e = +prompt("son kiriting");
+
+// let true;
+// let false;
+
+// if (a>=0 || b<-2) {
+//     true;
+// } else{
+// false;
+
+// }
+
+// console.log(true);
+// console.log(false);
+
+// 6-masala
+
+// let d = +prompt("son kiriting");
+// let e = +prompt("son kiriting");
+// let f = +prompt("son kiriting");
+
+// let true;
+// let false;
+
+// if (d<=e && b<=f) {
+//     true;
+// } else{
+// false;
+// }
+
+// console.log(true);
+// console.log(false);
+
+// 7-masala
+
+// let d = +prompt("son kiriting");
+// let e = +prompt("son kiriting");
+// let f = +prompt("son kiriting");
+
+// let true;
+// let false;
+
+// if (a>b>c) {
+//     true;
+// } else{
+// false;
+// }
+
+// console.log(true);
+// console.log(false);
+
+// 8-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// let toq_son;
+// let juft_son;
+
+// if (a % 2 ==1 && b % 2 ==1) {
+//     toq_son;
+// } else{
+// juft_son;
+// }
+
+// console.log(toq_son);
+// console.log(juft_son);
+
+// 9-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// let toq_son;
+// let juft_son;
+
+// if (a % 2 ==1 || b % 2 ==1) {
+//     toq_son;
+// } else{
+// juft_son;
+// }
+
+// console.log(toq_son);
+// console.log(juft_son);
+
+// 10-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// let toq_son;
+// let juft_son;
+
+// if (a % 2 ==1 || b % 2 ==1) {
+//     toq_son;
+// } else{
+// juft_son;
+// }
+
+// console.log(toq_son);
+// console.log(juft_son);
+
+// 11-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// let toq_son;
+// let juft_son;
+
+// if (a % 2 ==1 || b % 2 ==1) {
+//     toq_son;
+// } else{
+// juft_son;
+// }
+// if (a % 2 ==0 && b % 2 ==0) {
+//     juft_son;
+// } else{
+// toq_son;
+// }
+
+// console.log(toq_son);
+// console.log(juft_son);
+
+// 12-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+// let c = +prompt("son kiriting");
+
+// let musbat;
+// let manfiy;
+
+// if (a>0 && b>0 && c>0) {
+//     musbat;
+// } else{
+// manfiy;
+// }
+
+// console.log(musbat);
+// console.log(manfiy);
+
+// 13-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+// let c = +prompt("son kiriting");
+
+// let musbat;
+// let manfiy;
+
+// if (a>0 || b>0 || c>0) {
+//     musbat;
+// } else{
+// manfiy;
+// }
+
+// console.log(musbat);
+// console.log(manfiy);
+
+// 14-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+// let c = +prompt("son kiriting");
+
+// let musbat;
+// let manfiy;
+
+// if (a>0 || b>0 || c>0) {
+//     musbat;
+// } else{
+// manfiy;
+// }
+
+// console.log(musbat);
+// console.log(manfiy);
+
+// 15-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+// let c = +prompt("son kiriting");
+
+// let musbat;
+// let manfiy;
+
+// if (a>0 || b>0 && c>0) {
+//     musbat;
+// } else{
+// manfiy;
+// }
+
+// console.log(musbat);
+// console.log(manfiy);
+
+// 16-masala
+
+// let son = +prompt("a sonini kiriting");
+// if (son>9 && son<100 && son % 2 ==0) {
+//     console.log(true);
+// } else {
+// console.log(false);
+// }
+
